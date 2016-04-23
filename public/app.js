742af1a226d74df4d698d17ce3636919d0a92757
Carine TOdmia <ctodmia@gmail.com>
2016-04-23 09:02:22 -0700

    added initial code for app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,9 @@
-app.js
+angular.module('chitchat', ['ui.router'])
+	.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+			.state('home', {
+				url:'/',
+				templateUrl: 'views/chatBoxView.html', 
+				controller: 'ChatBoxController'
+			})
+	}])
---
+++ public/controllers/chatBoxController.js
@@ -0,0 +1,1 @@
+//this is where the chat box controller will live. 
